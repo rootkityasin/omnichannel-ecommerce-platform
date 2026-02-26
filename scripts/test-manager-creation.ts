cda0eefc7733328539d8606e1a1f505f7e238a2a
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2026-02-27 01:52:11 +0600

    chore: remove temporary debug script breaking next build

--- scripts/test-manager-creation.ts
+++
@@ -1,9 +0,0 @@
-import { createUserWithRole } from "./app/actions/user";
-
-async function testAuth() {
-  // We can't easily mock auth() globally.
-  // I need to write a standalone test or bypass the auth check entirely.
-  console.log("Starting test...");
-}
-
-testAuth();
